b7e349cb00b448e0790df1dcd6c444d16893e3c3
Lovro <mekis.lovro@gmail.com>
2016-03-28 20:02:38 +0000

    added routing

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 from flask import Flask, render_template, jsonify
-
+import re
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+    
+@app.route("/new/<new_url>")
+def new_link(new_url):
+    
+    http_valid_format = re.compile("(https?:\/\/\w+\.\w+.+)")
+    ##Check if new url has a valid format, if passes, check if it's already in the database
+    if http_valid_format.match(new_url):
+        
+        new_short_url = ""
+        
+        return jsonify(original_url = new_url, short_url = new_short_url)
+    else:
+        return jsonify(error = "Wrong url format, make sure you have a valid protocol and real site.")
+
+
+@app.route("/<int:id>")
+def get_link(id):
+    ##Check the databse for existing id and redirect : else return error
+    return jsonify(error = "This url is not in the database.")
 
 
 ### Runs server
